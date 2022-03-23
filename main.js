ab5ace87160392afc80bc973c264b7d4e0a3af47
Oliviahellquist <olivia.hellqvist@gmail.com>
2022-03-23 12:16:55 +0100

    dark and light theme
    Add a button that makes it possible to have a "Dark" and "Light" and saved in LocalStorage

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let selector = document.querySelector("#theme-selector");
+let cssLink = document.querySelector("#theme");
+
+selector.addEventListener("change", changeTheme);
+
+//make the function that change the theme 
+function changeTheme(){
+    cssLink.href = `${selector.value}.css`;
+    localStorage.setItem("theme", selector.value);
+}
+
+function setTheme(){
+    let theme = localStorage.getItem("theme");
+    console.log(theme);
+    cssLink.href = `${theme}.css`;
+}
+
+setTheme();
